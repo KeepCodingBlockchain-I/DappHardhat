d7ed9147dba51eb55dbf8edcf63225063cfe2825
SamuelZambrana <samu-1994@hotmail.es>
2024-02-08 19:10:48 +0100

    Modificado por samuel

--- scripts/ERC20.deploy.js
+++ scripts/ERC20.deploy.js
@@ -4,7 +4,13 @@ let deployedERC20Contract
 let contractAddress
 
 async function deploy(){
-    
+    console.log("ERC20 deployemet has just started..")
+    const ERC20Contract = await ethers.getContractFactory("MyCoin")
+    deployedERC20Contract = await ERC20Contract.deploy(5000,2)
+    await deployedERC20Contract.waitForDeployment()
+    contractAddress = deployedERC20Contract.target
+    console.log("...ERC20 constract has been deployed...")
+
 }
 
 async function verify(){}
